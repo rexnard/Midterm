b3211ab6baf30a88437084e80afa63f2baf0d2e4
rexnard <100132176+rexnard@users.noreply.github.com>
2022-03-24 22:05:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,463 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Poverty in the Philippines</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    
+        </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>Poverty in the Philippines</h1>
+                <div id="navigation">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="causes.html">CAUSES OF POVERTY</a></li>
+                        <li><a href="images.html">WHAT POVERTY LOOKS LIKE (IMAGES)</a></li>
+                        <li><a href="about.html">ABOUT POVERTY</a></li> 
+                    </ul>       
+        </div>
+        </div>
+        <div id="content">
+            
+            <div id="causes-wrapper">
+            <div id="causes-text">
+                <h1>The main causes of poverty in the country.</h1>
+               
+                
+                <li>Low to moderate economic growth for the past 40 years;</li>
+                <li>Low growth elasticity of poverty reduction;</li>
+                <li>Weakness in employment generation and the quality of jobs generated;</li>
+                <li>Failure to fully develop the agriculture sector;</li>
+                <li>High inflation during crisis periods;</li>
+                <li>High levels of population growth;</li>
+                <li>High and persistent levels of inequality (incomes and assets), which dampen the positive impacts of economic expansion; and
+recurrent shocks and exposure to risks such as economic crisis, conflicts, natural disasters,and "environmental poverty."</li>
+
+                </div>
+
+            </div>
+            </div>
+            
+            <div id="footer">
+            <h3>www.PovertyinthePhilippines.com</h3>
+            </div>
+        
+        
+        </div>
+        
+    </body>    
+    
+    
+    
+
+
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Poverty in the Philippines</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    
+        </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>Poverty in the Philippines</h1>
+                <div id="navigation">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="causes.html">CAUSES OF POVERTY</a></li>
+                        <li><a href="images.html">WHAT POVERTY LOOKS LIKE (IMAGES)</a></li>
+                        <li><a href="about.html">ABOUT POVERTY</a></li> 
+                    </ul>       
+        </div>
+        </div>
+        <div id="content">
+            
+            <div id="images-wrapper">
+                <div id="first-image">
+                    <img src="first-image.jpg" title="poverty" alt="poverty"/>
+                </div>
+                <div id="second-image">
+                    <img src="second-image.jpg" title="poverty" alt="poverty"/>
+                </div>
+                <div id="third-image">
+                    <img src="third-image.jpg" title="poverty" alt="poverty"/>
+                </div>
+                <div id="fourth-image">
+                    <img src="fourth-image.jpg" title="poverty" alt="poverty"/>
+                </div>
+                <div id="fifth-image">
+                    <img src="fifth-image.png" title="poverty" alt="poverty"/>
+                </div>
+                <div id="sixth-image">
+                    <img src="sixth-image.jpg" title="poverty" alt="poverty"/>
+                </div>
+            
+            </div>
+            </div>
+            
+            
+            
+            <div id="footer">
+            <h3>www.PovertyinthePhilippines.com</h3>
+            </div>
+        
+        
+        </div>
+        
+    </body>    
+    
+    
+    
+
+
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Poverty in the Philippines</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    
+        </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>Poverty in the Philippines</h1>
+                <div id="navigation">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="causes.html">CAUSES OF POVERTY</a></li>
+                        <li><a href="images.html">WHAT POVERTY LOOKS LIKE (IMAGES)</a></li>
+                        <li><a href="about.html">ABOUT POVERTY</a></li> 
+                    </ul>       
+        </div>
+        </div>
+        <div id="content">
+            <div id="content-wrapper">
+                <div id="about-text">
+                   <h1>POVERTY</h1>
+                    <p>With more than 16 percent of the population living in poverty, the Philippines has a pretty high poverty rate. About 17.6 million Filipinos are unable to buy basic essentials due to the large number of individuals who rely on agriculture for a living and wealth inequality. Poverty decreased from 21.6 percent to 16.6 percent between 2015 and 2020. President Rodrigo Duterte of the Philippines wants to reduce poverty to 14% by 2022. The Philippine government's AmBisyon 2040 initiative aims to eliminate extreme poverty by 2040. Furthermore, the government has adopted a number of programs and reforms aimed at reducing poverty, particularly in the areas of education, healthcare, and the economy as a whole. Here are five ways the program is helping the Philippines fight poverty.</p>
+
+                    
+                    <p>Population below minimum level of dietary energy consumption (also referred to as prevalence of undernourishment) shows the percentage of the population whose food intake is insufficient to meet dietary energy requirements continuously. Data showing as 5 may signify a prevalence of undernourishment below 5%.</p>
+                    
+                    <div id="box-one">
+                   <p class="text-content1"> <li>Philippines hunger statistics for 2018 was 14.50%, a 0.4% decline from 2017.</li> </p>
+                   <p class="text-content2"> <li>Philippines hunger statistics for 2017 was 14.90%, a 0.3% decline from 2016.</li></p>
+                   <p class="text-content3"> <li>Philippines hunger statistics for 2016 was 15.20%, a 1% decline from 2015.</li></p>
+                   <p class="text-content4"> <li>Philippines hunger statistics for 2015 was 16.20%, a 0.9% increase from 2014.</li></p>
+                    </div>
+                     
+                </div>
+                <div id="about-image">
+                <img src="about-poverty.jpg" alt="poverty" />                
+                </div>
+            </div>
+            
+            </div>
+            <div id="footer">
+            <h3>www.PovertyinthePhilippines.com</h3>
+            </div>
+        
+        
+        </div>
+        
+    </body>    
+    
+    
+    
+
+
+</html>
+body{
+    background-image: url(background.jpg);
+    background-size: cover;
+    
+    
+}
+
+#wrapper{
+    width: 1850px;
+    min-height: 1500;
+    margin: 0 auto;
+    padding: 5px;
+    background-color: rgba(64, 63, 63, 0.5); 
+    
+}
+#header{
+    width: 96%;
+    height: 150px;
+    margin: 0 auto;
+    padding: 20px;
+}
+#header h1{
+    color: #E7ECEA;
+    font-size: 60px;
+    position: relative;
+    left: 550px;   
+}
+#navigation{
+    width: 92%;
+    height: 50px;
+    float: right;    
+}
+#navigation ul{
+    list-style-type: none;
+    margin: 5;
+    padding: 0;
+}
+#navigation a{
+    min-width: 40px;
+    padding: 30px;
+    margin: 2px;
+    float: left;
+    font-size: 30px;
+    text-decoration: none;   
+}
+#navigation a:link, #navigation a:visited{
+    border: 3px solid #E7ECEA;
+    color: #E7ECEA;   
+}
+#navigation a:hover{
+    border: 2px solid #98A1A1;
+    color: #98A1A1; 
+    
+}
+#content{
+    width: 98%;
+    min-height: 900px;
+    padding: 5px;
+    margin: 10px;
+    display: block;
+    position: relative;
+    top: 300px;
+}
+#content #content-wrapper{
+    width: 98%;
+    min-height: 900px;
+    padding: 5px;
+    margin: 10px;
+    display: block;
+    position: relative;
+    bottom: 160px;
+}
+#content #content-wrapper #about-text{
+    width: 60%;
+    min-height: 600px;
+    padding: 5px;
+    margin: 10px;
+    display: inline-block;
+    position: relative;
+    bottom: 250px;
+}
+#content #content-wrapper #about-text p{
+    font-size: 21px;
+    color: aliceblue;
+}
+#content #content-wrapper #about-text h1{
+    font-size: 41px;
+    color: aliceblue;
+}
+#content #content-wrapper #about-image{
+    width: 30%;
+    padding: 5px;
+    margin: 10px;
+    display: inline-block; 
+    
+}
+
+#box-one {
+    border: solid white;
+    color: aliceblue
+}
+#content #causes-wrapper{
+    width: 96%;
+    min-height: 800px;
+    padding: 5px;
+    margin: 10px;
+    display: inline-block;
+    position: relative;
+    bottom: 250px;
+}
+#content #causes-wrapper #causes-text h1{
+    font-size: 41px;
+    color: aliceblue;
+}
+#content #causes-wrapper #causes-text li{
+    font-size: 21px;
+    color: aliceblue;
+    list-style-type: none;
+    margin: 0px;
+    padding 0px;
+}
+#content #abouts-wrapper{
+    width: 96%;
+    min-height: 800px;
+    padding: 5px;
+    margin: 10px;
+    display: inline-block; 
+    position: relative;
+    bottom: 250px;
+}
+#content #abouts-wrapper #abouts-text h1{
+    font-size: 41px;
+    color: aliceblue;
+}
+#content #abouts-wrapper #abouts-text p1{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #abouts-wrapper #abouts-text p2{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #abouts-wrapper #abouts-text p3{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #abouts-wrapper #abouts-text p4{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #abouts-wrapper #abouts-text p5{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #abouts-wrapper #abouts-text p6{
+    font-size: 21px;
+    color: aliceblue;
+    float: left;
+    padding: 10px;
+}
+#content #images-wrapper #first-image{
+    width: 100px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    right: 800px;
+}
+#content #images-wrapper #second-image{
+    width: 300px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    left: 100px;  
+    float: left;
+}
+#content #images-wrapper #third-image{
+    width: 300px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    left: 250px;
+}
+#content #images-wrapper #fourth-image{
+    width: 300px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    right: 20px;
+    bottom: 1050px;
+    left: 100px;
+}
+#content #images-wrapper #fifth-image{
+    width: 300px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    right: 600px;
+    bottom: 400px;
+}
+#content #images-wrapper #sixth-image{
+    width: 500px;
+    padding: 10px;
+    margin: 0 auto;
+    position: relative;
+    bottom: 900px;
+    left: 500px;
+}
+#footer{
+    width: 96%;
+    min-height: 50px;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+    display: block;    
+}
+#footer h3{
+    color: aliceblue;
+}
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Poverty in the Philippines</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    
+        </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>Poverty in the Philippines</h1>
+                <div id="navigation">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="causes.html">CAUSES OF POVERTY</a></li>
+                        <li><a href="images.html">WHAT POVERTY LOOKS LIKE (IMAGES)</a></li>
+                        <li><a href="about.html">ABOUT POVERTY</a></li> 
+                    </ul>       
+        </div>
+        </div>
+        <div id="content">
+            
+            <div id="abouts-wrapper">
+            <div id="abouts-text">
+                <h1>About Poverty</h1>
+                <p1><li> Poverty is defined as a lack of access to basic necessities such as food, water, clothing, and shelter. It is one of our society's problems.
+Rather, our country. Poverty affects people for a variety of causes, and it has a wide range of consequences.</li></p1>
+ <p2><li> There are several significant reasons why our country is experiencing poverty. One of the causes of this problem is the presence of irresponsible leaders or government officials. The problem of graft and corruption exists because, despite the passage of time, there are still authorities who use their position and power to obtain cash and utilize them for their own personal gain rather than to meet the needs of residents and our country. However, it is not always our government's responsibility; civilians can sometimes make mistakes. It's what we call overpopulation. It’s a cause of poverty because some couple or families are not having a 
+ proper family plan and nowadays, pregnancy happens at an early age that adds up to our population. The funds will not be enough 
+ to all of us that’s why some people will not be given the things that they need. Another reason that leads to poverty is the lack 
+ of opportunities for the people to work and have a higher salary.</li></p2>
+          <p3> <li> Because there are constraints and high standards for employment, a significant portion of our population is unable to work. These
+Nowadays, to be considered for a job, applicants must have completed college and have work experience. When they are not working, they perform menial tasks that do not pay well enough to meet their basic necessities. Finally, poverty is exacerbated by climate change. For me, it doesn’t affect all of the jobs but because there are so many hardworking farmers here in the Philippines that 
+work for their family, climate change is a big problem for them. When dry season arrives, farmers are having a hard time growing their 
+crops because their crops need enough amount of water to grow. When this happens, they will not produce products and will not earn money 
+that they will use to buy the needed things of their family that will add to their problems in farming.</li></p3>
+                <p4> <li> Poverty isn't only a squandered currency. It will have an impact not only on the residents, but on the entire country. Filipinos will, on the whole, have a positive outlook.
+Surviving in such trying circumstances is difficult. Furthermore, many people will become ill as a result of their excessive hunger and lack of energy.
+I'm going to work and won't be able to make enough money. When these things happen, our economy and country's progress will be slowed.</li></p4>
+                <p5><li>Everyone wants to live a happy life, which is why poverty must be avoided. 
+                    Every decision we make should be based on a strategy.
+will require, particularly when starting a family. You should also set a positive example for those around you if you become a leader. You must consider your options. Not only your own, but also the needs of others. More work opportunities are needed in our country, as well as support for pay parity.
+As a result, workers will receive a fair wage.</li></p5>
+                <p6><li> As a result, we will be able to understand that poverty is one of the global crises that requires immediate attention. Poverty is the state of being poor. The inability to meet our basic necessities in order to live longer. It's a lethal issue that's steadily murdering our population, and it's one that we, as the government, must address. Those who are affected must take action and find answers.</li></p6>
+                </div>
+            </div>
+            </div>
+            
+            <div id="footer">
+            <h3>www.PovertyinthePhilippines.com</h3>
+            </div>
+        
+        
+        </div>
+        
+    </body>    
+    
+    
+    
+
+
+</html>
